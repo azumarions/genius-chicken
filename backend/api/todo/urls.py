3128ef6a01d32827f21fb80d4997965e5f9643df
azuma <azuma20011224@gmail.com>
2023-01-22 20:06:22 +0900

    create api

---
+++ backend/api/todo/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+app_name = 'user'
+
+router = DefaultRouter()
+router.register('profile', views.ProfileviewSet, basename='profile')
+router.register('task', views.TaskViewSet, basename='task')
+
+
+urlpatterns = [
+    path('register/', views.CreateUserView.as_view(), name='register'),
+    path('myprofile/', views.MyProfileListView.as_view(), name='myprofile'),
+    path('profile-list/', views.ProfileListView.as_view(), name='profile-list'),
+    path('profile-detail/<slug:pk>/',
+         views.ProfileRetrieveView.as_view(), name='profile-detail'),
+    path('task-list/', views.TaskListView.as_view(), name='task-list'),
+    path('task-detail/<slug:pk>/',
+         views.TaskRetrieveView.as_view(), name='task-detail'),
+    path('', include(router.urls))
+]
--- backend/api/todo/views.py
+++ backend/api/todo/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny
+from . import serializers
+from .models import Profile, Task
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class ProfileviewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userProfile=self.request.user)
+
+
+class ProfileRetrieveView(generics.RetrieveAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+    permission_classes = (AllowAny,)
+
+
+class ProfileListView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+    permission_classes = (AllowAny,)
+
+
+class MyProfileListView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(userProfile=self.request.user)
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = serializers.TaskSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userTask=self.request.user)
+
+
+class TaskRetrieveView(generics.RetrieveAPIView):
+    queryset = Task.objects.all()
+    serializer_class = serializers.TaskSerializer
+    permission_classes = (AllowAny,)
+
+
+class TaskListView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = serializers.TaskSerializer
+    permission_classes = (AllowAny,)
