3523955a51fbbbe0cf3ae9207e79391f73faeda2
azuma <azuma20011224@gmail.com>
2023-01-24 16:40:20 +0900

    validation of auth func

--- frontend/client/src/components/Auth/index.tsx
+++ frontend/client/src/components/Auth/index.tsx
@@ -2,25 +2,50 @@ import React, { useState } from "react"
 import { useRouter } from "next/router"
 import Cookie from "universal-cookie"
 import { createProf, getProf, getMyProf } from "../../api/account";
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
+import { AUTH, SnackbarMessage } from "@/types";
+import { SubmitHandler, useForm } from "react-hook-form";
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { IconButton, Snackbar, Avatar, Button, TextField, Box, Typography, } from "@mui/material";
+import LoginIcon from '@mui/icons-material/Login';
+import CloseIcon from '@mui/icons-material/Close';
 
 const cookie = new Cookie();
 
 const Auth = () => {
   const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [isLogin, setIsLogin] = useState<boolean>(true);
+  const [isAuth, setIsAuth] = useState<boolean>(false); //global stateにする
+  const [snackPack, setSnackPack] = React.useState<readonly SnackbarMessage[]>([]);
+  const [open, setOpen] = React.useState(false);
+  const [messageInfo, setMessageInfo] = React.useState<SnackbarMessage | undefined>(undefined,);
+  const theme = createTheme({});
+  const {register, handleSubmit, formState: { errors },} = useForm<AUTH>();
 
-  const login = async () => {
+  React.useEffect(() => {
+    if (snackPack.length && !messageInfo) {
+      setMessageInfo({ ...snackPack[0] });
+      setSnackPack((prev) => prev.slice(1));
+      setOpen(true);
+    } else if (snackPack.length && messageInfo && open) {
+      setOpen(false);
+    }
+  }, [snackPack, messageInfo, open]);
+
+  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const handleExited = () => {
+    setMessageInfo(undefined);
+  };
+
+
+  const login = async (data: AUTH) => {
+    const { email, password } = data
     try {
       await fetch(
         `${process.env.NEXT_PUBLIC_RESTAPI_URL}/authen/jwt/create/`,
@@ -31,132 +56,138 @@ const Auth = () => {
             'Content-Type': 'application/json',
           },
         }
-      )
-      .then((res) => {
-        if (res.status === 400) {
-          throw 'authlization failed';
-        } else if (res.ok) {
-          return res.json();
+      ).then((res) => {
+          if (res.status === 401) {
+            setSnackPack((prev) => [...prev, { message: "メールかパスワードが違います。", key: new Date().getTime() }]);
+          } else if (res.ok) {
+            return res.json();
+          }
         }
-      })
-      .then((data) => {
+      ).then((data) => {
         const options = { path: '/' };
         cookie.set('access_token', data.access, options);
+        setIsAuth(true)
         // getProf();
       });
       router.push('/home');
     } catch (err) {
-      alert(err);
+      // alert(err);
     }
   };
 
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (isLogin) {
-      login();
+  const onSubmit: SubmitHandler<AUTH> = async (data) => {
+    if (isAuth === true) {
+      login(data);
     } else {
       try {
         await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}/api/register/`, {
           method: 'POST',
-          body: JSON.stringify({ email: email, password: password }),
+          body: JSON.stringify({ email: data.email, password:data.password }),
           headers: {
             'Content-Type': 'application/json',
           },
         }).then((res) => {
           if (res.status === 400) {
-            throw 'すでに登録されているメールアドレスです。ログインしてください。';
+            setSnackPack((prev) => [...prev, { message: "すでに登録されているメールアドレスです。ログインしてください。", key: new Date().getTime() }]);
           }
         });
-        await login();
+        await login(data);
         await createProf();
       } catch (err) {
-        alert(err);
+        // alert(err);
       }
     }
   }
 
-  const theme = createTheme({
-    typography: {
-      fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(','),
-    },
-  });
-
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 2,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          {isLogin ? <Avatar sx={{ m: 1, width: 150, height: 150 }} src="/auth_icon1.jpeg" alt="icon">
-          </Avatar> : <Avatar sx={{ m: 1, width: 150, height: 150 }} src="/auth_icon2.jpeg" alt="icon">
-          </Avatar> }
-          
-          <Typography component="h1" variant="h5" fontFamily="serif">
-            {isLogin? 'Log in' : 'Sign up'}
-          </Typography>
-          <Box component="form" onSubmit={onSubmit} method="POST" noValidate sx={{ mt: 1 }}>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        {isLogin ? <Avatar sx={{ m: 1, width: 150, height: 150 }} src="/auth_icon1.jpeg" alt="icon">
+        </Avatar> : <Avatar sx={{ m: 1, width: 150, height: 150 }} src="/auth_icon2.jpeg" alt="icon">
+        </Avatar> }
+        
+        <Typography component="h1" variant="h5" fontFamily="serif">
+          {isLogin? 'Log in' : 'Sign up'}
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit(onSubmit)} method="POST" sx={{ width: "80%"}}>
+          <Box sx={{ mt: 1 }}>
             <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="メールアドレス"
-              name="email"
+              {...register("email", { required: true })}
               type="email"
-              autoComplete="email"
+              fullWidth
+              placeholder="メールアドレス"
+              variant="outlined"
+              size="small"
               autoFocus
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value)
-              }}
             />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              label="パスワード"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value)
-              }}
-            />
-            <Grid container justifyItems="center" alignItems="center">
-              <Grid item sx={{ mt: 1, mb: 0 }} xs={4} onClick={() => setIsLogin(!isLogin)}>
-                {isLogin ? '新規登録へ' : 'ログインへ'}
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2, backgroundColor: "black" }}
-            >
-              {isLogin ? 'ログイン' : '新規登録'}
-            </Button>  
+            {errors.email && (
+            <Box component="p" sx={{ fontSize: 10, color: "red"}}>
+              メールアドレスは必須です
+            </Box>)}
           </Box>
+          <Box sx={{ mt: 1 }}>
+            <TextField   
+            {...register("password", { required: true })}
+            type="password"
+            fullWidth
+            placeholder="パスワード"
+            variant="outlined"
+            size="small"
+            />
+            {errors.email && (
+            <Box component="p" sx={{ fontSize: 10, color: "red"}}>
+              パスワードは必須です
+            </Box>)}
+          </Box>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 2, mb: 1, backgroundColor: "black" }}
+          >
+            {isLogin ? 'ログイン' : '新規登録'}
+          </Button>  
         </Box>
-      </Container>
+        <Snackbar
+          ContentProps={{sx: {
+            background: "orange"
+          }}}
+          key={messageInfo ? messageInfo.key : undefined}
+          open={open}
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+          autoHideDuration={10000}
+          onClose={handleClose}
+          TransitionProps={{ onExited: handleExited }}
+          action={
+            <React.Fragment>
+              <Button color="inherit" size="small" onClick={handleClose}>
+                {messageInfo?.message}
+              </Button>
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                sx={{ p: 0.1 }}
+                onClick={handleClose}
+              >
+                <CloseIcon />
+              </IconButton>
+            </React.Fragment>
+          }
+        />
+        <Box>
+          <Button sx={{ mt: 1, mb: 3, textAlign: "right" }} onClick={() => setIsLogin(!isLogin)}>
+            {isLogin ? '新規登録へ' : 'ログインへ'}<LoginIcon />
+          </Button>
+        </Box>
+      </Box>
     </ThemeProvider>
   )
 }
