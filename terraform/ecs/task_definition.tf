26c78a88669e4c6ac02aebcf9d1fe7dc0d1db435
azuma <azuma20011224@gmail.com>
2023-02-14 20:16:01 +0900

    create terraform RDS, ECR, ECS

---
+++ terraform/ecs/task_definition.tf
@@ -0,0 +1,19 @@
+resource "aws_ecs_task_definition" "example" {
+  family                   = "example"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./container_definitions.json")
+}
+
+# cloud watch log
+# resource "aws_ecs_task_definition" "example" {
+#   family                   = "example"
+#   cpu                      = "256"
+#   memory                   = "512"
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   container_definitions    = file("./container_definitions.json")
+#   execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+# }
---
+++ terraform/rds/main.tf
@@ -0,0 +1,92 @@
+resource "aws_db_parameter_group" "example" {
+  name   = "example"
+  family = "mysql5.7"
+
+  parameter {
+    name  = "character_set_database"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+}
+
+resource "aws_db_option_group" "example" {
+  name                 = "example"
+  engine_name          = "mysql"
+  major_engine_version = "5.7"
+
+  option {
+    option_name = "MARIADB_AUDIT_PLUGIN"
+  }
+}
+
+resource "aws_db_subnet_group" "example" {
+  name       = "example"
+  subnet_ids = [aws_subnet.private_0.id, aws_subnet.private_1.id]
+}
+
+resource "aws_db_instance" "example" {
+  identifier                 = "example"
+  engine                     = "mysql"
+  engine_version             = "5.7.25"
+  instance_class             = "db.t3.small"
+  allocated_storage          = 20
+  max_allocated_storage      = 100
+  storage_type               = "gp2"
+  storage_encrypted          = true
+  kms_key_id                 = aws_kms_key.example.arn
+  username                   = "admin"
+  password                   = "VeryStrongPassword!"
+  multi_az                   = true
+  publicly_accessible        = false
+  backup_window              = "09:10-09:40"
+  backup_retention_period    = 30
+  maintenance_window         = "mon:10:10-mon:10:40"
+  auto_minor_version_upgrade = false
+  deletion_protection        = true
+  skip_final_snapshot        = false
+  port                       = 3306
+  apply_immediately          = false
+  vpc_security_group_ids     = [module.mysql_sg.security_group_id]
+  parameter_group_name       = aws_db_parameter_group.example.name
+  option_group_name          = aws_db_option_group.example.name
+  db_subnet_group_name       = aws_db_subnet_group.example.name
+
+  lifecycle {
+    ignore_changes = [password]
+  }
+}
+
+module "mysql_sg" {
+  source      = "./security_group"
+  name        = "mysql-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 3306
+  cidr_blocks = [aws_vpc.example.cidr_block]
+}
+
+resource "aws_elasticache_subnet_group" "example" {
+  name       = "example"
+  subnet_ids = [aws_subnet.private_0.id, aws_subnet.private_1.id]
+}
+
+resource "aws_elasticache_replication_group" "example" {
+  replication_group_id          = "example"
+  replication_group_description = "Cluster Disabled"
+  engine                        = "redis"
+  engine_version                = "5.0.4"
+  number_cache_clusters         = 3
+  node_type                     = "cache.m3.medium"
+  snapshot_window               = "09:10-10:10"
+  snapshot_retention_limit      = 7
+  maintenance_window            = "mon:10:40-mon:11:40"
+  automatic_failover_enabled    = true
+  port                          = 6379
+  apply_immediately             = false
+  security_group_ids            = [module.redis_sg.security_group_id]
+  parameter_group_name          = aws_elasticache_parameter_group.example.name
+  subnet_group_name             = aws_elasticache_subnet_group.example.name
+}
