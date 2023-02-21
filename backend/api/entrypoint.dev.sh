74bf98785ea802b0f3300f32e15806852a108e5e
azuma <azuma20011224@gmail.com>
2023-02-21 22:22:57 +0900

    test

--- backend/api/entrypoint.dev.sh
+++ backend/api/entrypoint.dev.sh
@@ -18,6 +18,7 @@
 #     cd usr/src/app && gunicorn config.wsgi:application --bind 0.0.0.0:8000
 # fi
 
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+# gunicorn config.wsgi:application --bind 0.0.0.0:8000
+cd usr/src/app && gunicorn config.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
